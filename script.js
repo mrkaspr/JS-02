bacda26dcafcbb12a13d2688047abb6dd9e52740
Kay <kreshna.sp@gmail.com>
2020-06-14 20:26:54 +1000

    Adding coding challange 3

--- script.js
+++ script.js
@@ -83,3 +83,21 @@ if (johnAverage > mikeAverage && johnAverage > maryAverage){
 //note: I wonder if I can add a test to check whether the logic is correct
 
 
+/*
+
+Coding Challange #3
+
+John and his family went on a holiday and went to 3 different restaurants. The bill were #124, $48 and $268
+
+To tip the waiter a fair amound, John created a simple tip calculator (as a function). He likes to tip 
+20% of the bill when the bill is less than $50, 
+15% of the bill is between $50 and $200,
+and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays,
+1) Containing all three tips
+2) Containing all three final paid amounts (bill + tip).
+
+
+
+*/
